20be82a8acf60be5d13b2335cb9666ea2c445eb7
Jakub <jakub.gredzicki@apator.com>
2025-02-11 14:37:45 +0100

    moved main includes to separate file

---
+++ includes.h
@@ -0,0 +1,33 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/uart.h"
+#include "pico/multicore.h"
+#include "event_manager.h"
+#include "PowerManager.h"
+
+#include "ISensor.h"
+#include "lib/BH1750/BH1750_WRAPPER.h"
+#include "lib/BME280/BME280_WRAPPER.h"
+#include "lib/DS3231/DS3231.h"
+#include "lib/HMC5883L/HMC5883L_WRAPPER.h"
+#include "lib/LoRa/LoRa-RP2040.h"
+#include <iostream>
+#include <iomanip>
+#include <queue>
+#include <chrono>
+#include "protocol.h"
+#include "sd_card.h"
+#include "ff.h"
+#include <atomic>
+#include <iostream>
+#include <map>
+#include "pin_config.h"
+#include "storage.h"
+#include "utils.h"
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,31 +1,4 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/spi.h"
-#include "hardware/i2c.h"
-#include "hardware/uart.h"
-#include "pico/multicore.h"
-#include "event_manager.h"
-#include "PowerManager.h"
-
-#include "ISensor.h"
-#include "lib/BH1750/BH1750_WRAPPER.h"
-#include "lib/BME280/BME280_WRAPPER.h"
-#include "lib/DS3231/DS3231.h"
-#include "lib/HMC5883L/HMC5883L_WRAPPER.h"
-#include "lib/LoRa/LoRa-RP2040.h"
-#include <iostream>
-#include <iomanip>
-#include <queue>
-#include <chrono>
-#include "protocol.h"
-#include "sd_card.h"
-#include "ff.h"
-#include <atomic>
-#include <iostream>
-#include <map>
-#include "pin_config.h"
-#include "storage.h"
-#include "utils.h"
+#include "includes.h"
 
 #define LOG_FILENAME "log.txt"
 
