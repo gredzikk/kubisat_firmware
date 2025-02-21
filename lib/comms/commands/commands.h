c337a75ef35eca8dc5f1558559dcb426e4c6eef1
Jakub <jakub.gredzicki@apator.com>
2025-02-21 10:32:48 +0100

    cosmetic changes

--- lib/comms/commands/commands.h
+++ lib/comms/commands/commands.h
@@ -7,11 +7,13 @@
 #include <map>
 #include "protocol.h"
 
+
 // CLOCK
 Frame handle_time(const std::string& param, OperationType operationType);
 Frame handle_timezone_offset(const std::string& param, OperationType operationType);
 Frame handle_clock_sync_interval(const std::string& param, OperationType operationType);
 Frame handle_get_last_sync_time(const std::string& param, OperationType operationType);
+
 
 // DIAG
 Frame handle_get_commands_list(const std::string& param, OperationType operationType);
@@ -19,11 +21,13 @@ Frame handle_get_build_version(const std::string& param, OperationType operation
 Frame handle_verbosity(const std::string& param, OperationType operationType);
 Frame handle_enter_bootloader_mode(const std::string& param, OperationType operationType);
 
+
 // GPS
 Frame handle_gps_power_status(const std::string& param, OperationType operationType);
 Frame handle_enable_gps_uart_passthrough(const std::string& param, OperationType operationType);
 Frame handle_get_rmc_data(const std::string& param, OperationType operationType);
 Frame handle_get_gga_data(const std::string& param, OperationType operationType);
+
 
 // POWER
 Frame handle_get_power_manager_ids(const std::string& param, OperationType operationType);
@@ -33,6 +37,7 @@ Frame handle_get_current_charge_usb(const std::string& param, OperationType oper
 Frame handle_get_current_charge_solar(const std::string& param, OperationType operationType);
 Frame handle_get_current_charge_total(const std::string& param, OperationType operationType);
 Frame handle_get_current_draw(const std::string& param, OperationType operationType);
+
 
 // EVENT
 Frame handle_get_last_events(const std::string& param, OperationType operationType);
